d3a67096dfd343604d38c7a48324f523bce80af1
rbocaz <rbocaz@gmail.com>
2024-08-08 16:33:43 -0400

    agrega buildspec

--- app-cdk/lib/pipeline-cdk-stack.ts
+++ app-cdk/lib/pipeline-cdk-stack.ts
@@ -13,8 +13,13 @@ export class MyPipelineStack extends cdk.Stack {
     const githubSecret = secretsmanager.Secret.fromSecretNameV2(this, 'GitHubSecret', 'github/workshop_cicd');
 
     // Crea un proyecto de CodeBuild
-    const buildProject = new codebuild.PipelineProject(this, 'BuildProject', {
-      buildSpec: codebuild.BuildSpec.fromSourceFilename('buildspec.yml'),
+    const codeBuild = new codebuild.PipelineProject(this, 'CodeBuild', {
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_7_0,
+        privileged: true,
+        computeType: codebuild.ComputeType.LARGE,
+      },
+      buildSpec: codebuild.BuildSpec.fromSourceFilename('buildspec_test.yml'),
     });
 
     // Define los artefactos
@@ -23,7 +28,8 @@ export class MyPipelineStack extends cdk.Stack {
 
     // Define el pipeline
     const pipeline = new codepipeline.Pipeline(this, 'Pipeline', {
-      pipelineName: 'MyPipeline',
+      pipelineName: 'CICD_Pipeline',
+      crossAccountKeys: false,
     });
 
     // Agrega la etapa de origen con GitHub
@@ -47,7 +53,7 @@ export class MyPipelineStack extends cdk.Stack {
       actions: [
         new codepipeline_actions.CodeBuildAction({
           actionName: 'Build',
-          project: buildProject,
+          project: codeBuild,
           input: sourceOutput,
           outputs: [buildOutput],
         }),
