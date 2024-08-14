92af6db4921533789258985fd5dcfc40e2b6f490
rbocaz <rbocaz@gmail.com>
2024-08-14 19:23:05 -0400

    added test environment

--- app-cdk/bin/app-cdk.ts
+++ app-cdk/bin/app-cdk.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { MyPipelineStack } from '../lib/pipeline-cdk-stack';
 import { EcrCdkStack } from '../lib/ecr-cdk-stack';
+import { AppCdkStack } from '../lib/app-cdk-stack';
 
 const app = new cdk.App();
 const ecrCdkStack = new EcrCdkStack(app, 'ecr-stack', {});
@@ -8,3 +9,6 @@ new MyPipelineStack(app, 'MyPipelineStack', {
     ecrRepository: ecrCdkStack.repository,
   });
 
+  const testCdkStack = new AppCdkStack(app, 'test', {
+    ecrRepository: ecrCdkStack.repository,
+  });
